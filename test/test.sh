7b5bd06a695f859f147a6b09ec0b97c0567902f8
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2022-02-27 13:48:42 +0100

    update

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,8 @@
 # This script provides the functionality to test Kakoune scripts.
 # Tests live in the `test` directory and must end with `_test.kak`.
 
+# Temporary files
+# Prepare an output file to retrieve data from Kakoune.
 tmp=$(mktemp -d)
 log_path=$tmp/kakoune.log
 at_exit() {
@@ -8,6 +10,7 @@ at_exit() {
 }
 trap at_exit EXIT
 
+# Run tests
 kak -n -ui dummy -e "source test/test.kak $log_path" < /dev/null > /dev/null 2>&1 &
 kak_pid=$!
 wait "$kak_pid"
